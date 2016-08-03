4f2656800255ff49a71a3237c49b3d191ea932fe
LittleCoder <i7meavnktqegm1b@qq.com>
2016-08-03 12:08:48 +0800

    Add __init__ division

--- danmu/__init__.py
+++ danmu/__init__.py
@@ -1,1 +1,16 @@
 __version__ = '1.0.0'
+
+from DouYu import DouYuDanMuClient
+from Panda import PandaDanMuClient
+from ZhanQi import ZhanQiDanMuClient
+from QuanMin import QuanMinDanMuClient
+
+def DanMuClient(url):
+    for u in (('panda.tv', PandaDanMuClient),
+            ('douyu.com', DouYuDanMuClient),
+            ('quanmin.tv', QuanMinDanMuClient),
+            ('zhanqi.tv', ZhanQiDanMuClient)):
+        r = re.match('.*?%s/*.*?/[^/]*?$'%u[0], url)
+        if r:
+            if 'http://' == url[:7]: return u[1](url)
+            return u[1]('http://' + url)

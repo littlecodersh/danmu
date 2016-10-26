59d405eaf4b5e0a48b51df6c51715187aac62ad4
LittleCoder <i7meavnktqegm1b@qq.com>
2016-10-27 00:55:38 +0800

    Add HuoMao live detect

--- danmu/HuoMao.py
+++ danmu/HuoMao.py
@@ -3,7 +3,62 @@ from struct import pack
 
 import requests
 
-roomId = '717'
+from .Abstract import AbstractDanMuClient
+
+class BilibiliDanMuClient(AbstractDanMuClient):
+    def _get_live_status(self):
+        url = ('http://live.bilibili.com/'
+            + self.url.split('/')[-1] or self.url.split('/')[-2])
+        self.roomId = re.findall(b'var ROOMID = (\d+);', requests.get(url).content)[0].decode('ascii')
+        r = requests.get('http://live.bilibili.com/api/player?id=cid:' + self.roomId)
+        self.serverUrl = re.findall(b'<server>(.*?)</server>', r.content)[0].decode('ascii')
+        return re.findall(b'<state>(.*?)</state>', r.content)[0] == b'LIVE'
+    def _prepare_env(self):
+        return (self.serverUrl, 788), {}
+    def _init_socket(self, danmu, roomInfo):
+        self.danmuSocket = _socket()
+        self.danmuSocket.connect(danmu)
+        self.danmuSocket.settimeout(3)
+        self.danmuSocket.push(data = json.dumps({
+            'roomid': int(self.roomId),
+            'uid': int(1e14 + 2e14 * random.random()),
+            }, separators=(',', ':')).encode('ascii'))
+    def _create_thread_fn(self, roomInfo):
+        def keep_alive(self):
+            self.danmuSocket.push(b'', 2)
+            time.sleep(30)
+        def get_danmu(self):
+            if not select.select([self.danmuSocket], [], [], 1)[0]: return
+            content = self.danmuSocket.pull()
+            for msg in re.findall(b'\x00({[^\x00]*})', content):
+                try:
+                    msg = json.loads(msg.decode('utf8', 'ignore'))
+                    msg['NickName'] = (msg.get('info', ['','',['', '']])[2][1]
+                        or msg.get('data', {}).get('uname', ''))
+                    msg['Content']  = msg.get('info', ['', ''])[1]
+                    msg['MsgType']  = {'SEND_GIFT': 'gift', 'DANMU_MSG': 'danmu',
+                        'WELCOME': 'enter'}.get(msg.get('cmd'), 'other')
+                except Exception as e:
+                    pass
+                else:
+                    self.danmuWaitTime = time.time() + self.maxNoDanMuWait
+                    self.msgPipe.append(msg)
+        return get_danmu, keep_alive # danmu, heart
+
+url = 'http://www.huomao.com/%s'
+roomId = '1745'
+
+c = requests.get(url % roomId).content
+r = re.search('getFlash\("(.*?)","(.*?)"\);', c)
+data = {
+    'cid': r.group(1),
+    'cdns': '1',
+    'streamtype': 'live',
+    'VideoIDS': r.group(2), }
+j = requests.post('http://www.huomao.com/swf/live_data', data=data).json()
+print(j['roomStatus'] == '1')
+
+roomId = '11619'
 
 url = 'http://chat.huomao.com/chat/getToken'
 params = {
@@ -37,15 +92,15 @@ data = json.dumps(data, separators=(',', ':'))
 push_data(data)
 print(repr(s.recv(999)))
 
-s.sendall(b'\x02\x00\x00\x00\x04\x00\x00\x4d\x00\x01\x20')
+s.sendall(b'\x02\x00\x00\x00')
 
 data = {
     'channelId': roomId,
     'log': True,
     'userId': '', }
-data = 'gate.gateHandler.lookupConnector'\
+data = '\x00\x01\x20gate.gateHandler.lookupConnector'\
     + json.dumps(data, separators=(',', ':'))
-s.sendall(data)
+push_data(data, b'\x04')
 r = s.recv(999)[6:]
 print(r)
 newDes = json.loads(r)
